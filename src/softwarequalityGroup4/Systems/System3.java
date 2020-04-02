dc0be3e014eea6ef4096d0e722927d3e8b8127f1
marvi <marvi@DESKTOP-9TV7N5C>
2020-04-02 13:16:14 +0200

    System3 added

---
+++ src/softwarequalityGroup4/Systems/System3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwarequalityGroup4.Systems;
+
+/**
+ *
+ * @author marvi
+ */
+public class System3 {
+    
+}
