1cfe76a57037d424e7385dd852cc0446fa6cd52e
marvi <marvi@DESKTOP-9TV7N5C>
2020-04-02 15:57:24 +0200

    System3 updated

--- src/softwarequalityGroup4/Systems/System3.java
+++ src/softwarequalityGroup4/Systems/System3.java
@@ -12,9 +12,11 @@ package softwarequalityGroup4.Systems;
 public class System3 extends SQSystem{
 
     double t = 10.0;
-
+    Boolean falsification;
+    
     public System3(int n, double[] lambdas, Boolean falsification) {
         super(n, lambdas, falsification);
+        this.falsification = falsification;
     }
     
     public double getReliabilityParameter(){
@@ -31,12 +33,23 @@ public class System3 extends SQSystem{
             
             double Ri = Math.exp(-(lambdas[i])*t);
             
-            if(i == 0){
-                Fp = 1.0 - Ri;
+            if(!falsification){
+                if(i == 0){
+                    Fp = 1.0 - Ri;
+                }
+                else{
+                    Fp *= (1.0 - Ri); 
+                } 
             }
             else{
-                Fp *= (1.0 - Ri); 
+                if(i == 0){
+                    Fp = 0.8 - Ri;
+                }
+                else{
+                    Fp *= (0.8 - Ri); 
+                } 
             }
+
              
         }
         
