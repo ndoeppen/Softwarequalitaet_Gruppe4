d0c9bcf582b6f6d534ad4e79520e55b30b81ed8c
KuhnFinn <finqun@web.de>
2020-04-02 15:38:36 +0200

    Now everything works fine

--- src/softwarequalityGroup4/Systems/System1.java
+++ src/softwarequalityGroup4/Systems/System1.java
@@ -1,42 +1,38 @@
 package softwarequalityGroup4.Systems;
 
-public class System1 implements ISoftwareQualitySystem
-{
+public class System1 extends SQSystem {
 
-    int components;
-    double[] lambdas;
+
     final int t;
 
-    public System1(int pComponents, double[] pLambdas) throws Exception
-    {
-        components =pComponents;
-        lambdas = pLambdas;
-        t = 10;
-
-        if(pComponents!=pLambdas.length){
+    public System1(int pComponents, double[] pLambdas, Boolean falsification) throws Exception {
+        super(pComponents, pLambdas, falsification);
+        if (falsification) {
+            t = 5000;
+        } else {
+            t = 10;
+        }
+        if (pComponents != pLambdas.length) {
             throw new Exception("n!=length");
         }
     }
 
     @Override
-    public double getReliabilityParameter()
-    {
-        double fP = 1-product(lambdas);
+    public double getReliabilityParameter() {
+        double fP = 1 - product(lambdas);
         return fP;
     }
 
-    private double compReliability (double lambda)
-    {
+    private double compReliability(double lambda) {
         double rI;
-        rI=Math.exp(-(lambda)*t);
+        rI = Math.exp(-(lambda) * t);
         return rI;
     }
 
-    private double product(double[] pFactors)
-    {
-        double product=1;
-        for(int i=0;i<pFactors.length;i++){
-            product = product *(1-compReliability(pFactors[i]));
+    private double product(double[] pFactors) {
+        double product = 1;
+        for (int i = 0; i < pFactors.length; i++) {
+            product = product * (1 - compReliability(pFactors[i]));
         }
         return product;
     }
