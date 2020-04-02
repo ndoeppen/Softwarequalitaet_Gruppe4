d22f1ed2ea814b8564572f58b0acee2259fafaa0
Torsten Barkhüser <tor.bark@web.de>
2020-04-02 14:53:19 +0200

    System2 added

--- src/softwarequalityGroup4/Systems/System2.java
+++ src/softwarequalityGroup4/Systems/System2.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package softwarequalityGroup4.Systems;
 
 /**
- *
- * @author Nils
+ * Lambda in pro Jahr
+ * @author 9543450
  */
-public class System2 {
+public class System2 implements SoftwareQualitySystem{
+    
+    private int anzahlAusfallraten;
+    private double ausfallraten[];
+    private double zuverlaessigkeit;
+    private static int t;
+    
+    public System2(int pAnzahlAusfallraten, double pAusfallraten[]) {
+        
+        t = 10;
+        anzahlAusfallraten = pAnzahlAusfallraten;
+        ausfallraten = pAusfallraten.clone();
+        
+        double[] r = new double[anzahlAusfallraten];
+        int i = 0;
+        for (double lambda : ausfallraten) {
+            r[i] = Math.exp(-lambda * t);
+            i++;
+        }
+        
+        double ausfallwahrscheinlichkeit = 1;
+        for(double ri : r){
+            ausfallwahrscheinlichkeit *= (1 - ri);
+        }
+        
+        zuverlaessigkeit = 1 - ausfallwahrscheinlichkeit;
+    }
+
+    @Override
+    public double getReliabilityParameter() {
+        return zuverlaessigkeit;
+    }
+    
+    public static void main(String[] args) {
+        double[] lambdas = {0.2, 0.2};
+        System2 test = new System2(lambdas.length, lambdas);
+        System.out.println(test.getReliabilityParameter());
+    }
     
 }
